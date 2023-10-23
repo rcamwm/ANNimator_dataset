795111ca9dc4562a02e15a89329a1a7f64ef9d84
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-10-23 02:19:44 -0700

    Update frame_dataset.py

--- frame_dataset.py
+++ frame_dataset.py
@@ -20,21 +20,21 @@ class FrameDataset(Dataset):
         between_frame = to_tensor(Image.open(os.path.join(self.folder_name, self.between_filenames[index])))
         return key_frame_1, key_frame_2, between_frame
 
-    @staticmethod
-    def save_pickle(dataset, path):
-        with open(path, 'wb') as file:
-            pickle.dump(dataset, file)
     
-    @staticmethod
-    def get_data_loaders(dataset_path, batchsize=1, train_percent=1):
-        with open(dataset_path, 'rb') as file:
-            dataset = pickle.load(file)
+def save_pickle(dataset, path):
+    with open(path, 'wb') as file:
+        pickle.dump(dataset, file)
+    
+   
+def get_data_loaders(dataset_path, batchsize=1, train_percent=1):
+    with open(dataset_path, 'rb') as file:
+        dataset = pickle.load(file)
 
-        dataset_size = len(dataset)
-        train_size = int(train_percent * dataset_size) 
-        test_size = dataset_size - train_size
-        train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    dataset_size = len(dataset)
+    train_size = int(train_percent * dataset_size) 
+    test_size = dataset_size - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
-        train_loader = DataLoader(train_dataset, batch_size=batchsize, shuffle=True)
-        test_loader = DataLoader(test_dataset, batch_size=batchsize, shuffle=False)
-        return train_loader, test_loader
+    train_loader = DataLoader(train_dataset, batch_size=batchsize, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batchsize, shuffle=False)
+    return train_loader, test_loader
