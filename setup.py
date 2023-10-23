a346976505a7a5fb0ae5ce5798027358c458fe40
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-10-23 02:33:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "torch==1.7.1",
-        "torchvision==0.8.2",
-        "Pillow==7.2.0",
+        "torch",
+        "torchvision",
+        "Pillow",
     ],
 )
