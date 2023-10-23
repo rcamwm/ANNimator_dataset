a8984a78f043c7ac9c21d38430f030e2657296b9
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-10-23 02:46:37 -0700

    Separate functions from class

---
+++ ANNimator_dataset/ds_functions.py
@@ -0,0 +1,20 @@
+from . import pickle
+from . import DataLoader, random_split
+
+def save_pickle(dataset, path):
+    with open(path, 'wb') as file:
+        pickle.dump(dataset, file)
+    
+   
+def get_data_loaders(dataset_path, batchsize=1, train_percent=1):
+    with open(dataset_path, 'rb') as file:
+        dataset = pickle.load(file)
+
+    dataset_size = len(dataset)
+    train_size = int(train_percent * dataset_size) 
+    test_size = dataset_size - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batchsize, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batchsize, shuffle=False)
+    return train_loader, test_loader
--- ANNimator_dataset/frame_dataset.py
+++ ANNimator_dataset/frame_dataset.py
@@ -1,7 +1,6 @@
 from . import os
-from . import pickle
 from . import transforms
-from . import Dataset, DataLoader, random_split
+from . import Dataset
 from . import Image
 
 class FrameDataset(Dataset):
@@ -19,22 +18,3 @@ class FrameDataset(Dataset):
         key_frame_2 = to_tensor(Image.open(os.path.join(self.folder_name, self.key_filenames[index][1])))
         between_frame = to_tensor(Image.open(os.path.join(self.folder_name, self.between_filenames[index])))
         return key_frame_1, key_frame_2, between_frame
-
-    
-def save_pickle(dataset, path):
-    with open(path, 'wb') as file:
-        pickle.dump(dataset, file)
-    
-   
-def get_data_loaders(dataset_path, batchsize=1, train_percent=1):
-    with open(dataset_path, 'rb') as file:
-        dataset = pickle.load(file)
-
-    dataset_size = len(dataset)
-    train_size = int(train_percent * dataset_size) 
-    test_size = dataset_size - train_size
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-
-    train_loader = DataLoader(train_dataset, batch_size=batchsize, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batchsize, shuffle=False)
-    return train_loader, test_loader
