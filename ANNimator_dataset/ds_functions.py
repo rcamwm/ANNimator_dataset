7c03f888c78394fb5717117654da4bfec4d5cb7f
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-10-23 19:52:20 -0700

    Update ds_functions.py

--- ANNimator_dataset/ds_functions.py
+++ ANNimator_dataset/ds_functions.py
@@ -5,17 +5,18 @@ def save_pickle(dataset, path):
     with open(path, 'wb') as file:
         pickle.dump(dataset, file)
 
-def get_dataset(dataset_path, train_percent=1):
+def get_dataset(dataset_path):
     with open(dataset_path, 'rb') as file:
         dataset = pickle.load(file)
-
+    return dataset
+   
+def get_data_loaders(dataset_path, batchsize=1, train_percent=1):
+    dataset = get_dataset(dataset_path)
     dataset_size = len(dataset)
     train_size = int(train_percent * dataset_size) 
     test_size = dataset_size - train_size
-    return random_split(dataset, [train_size, test_size])
-   
-def get_data_loaders(dataset_path, batchsize=1, train_percent=1):
-    train_dataset, test_dataset = get_dataset(dataset_path, train_percent)
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
     train_loader = DataLoader(train_dataset, batch_size=batchsize, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batchsize, shuffle=False)
     return train_loader, test_loader 
